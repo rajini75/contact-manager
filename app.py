24d1e4ce7ed1df335a217bb8f1c9e836dd12a0e9
rajini75 <your-github-email@example.com>
2026-03-19 20:10:58 +0530

    Fixed Render port issue

--- app.py
+++ app.py
@@ -60,4 +60,8 @@ def edit(id):
     conn.close()
     return render_template("edit.html", contact=contact)
 
-app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
